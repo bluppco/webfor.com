56c6d671e25a8041619f09205317ccf1f4c720c8
Shiva pandey <shivapandey9898@gmail.com>
2024-07-23 16:14:37 +0530

    feat: added  config for footer content

---
+++ src/content/config.js
@@ -0,0 +1,27 @@
+import { z, defineCollection } from "astro:content"
+
+const footerCollection = defineCollection({
+
+    schema: z.object({
+
+        title: z.string(),
+        order: z.number(),
+        items: z.array(
+
+            z.object({
+				title: z.string(),
+                order: z.number(),
+				link: z.string(),
+			})
+
+        ).optional()
+
+    })
+
+})
+
+export const collections = {
+
+    "footer": footerCollection,
+
+}

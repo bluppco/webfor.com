e89134a8eb91728736277582815a4aadb0187895
Shiva pandey <shivapandey9898@gmail.com>
2024-07-24 10:21:03 +0530

    refactor: rearanged the array

--- src/components/common/footer/socials/jsx/index.jsx
+++ src/components/common/footer/socials/jsx/index.jsx
@@ -6,11 +6,31 @@ import LinkNewWindow from "@/atoms/links/new-window/jsx"
 
 const socialData = [
 
-    { icon: <FaTwitter />, label: "Twitter", link: "/" },
-    { icon: <FaLinkedin />, label: "LinkedIn", link: "/" },
-    { icon: <FaInstagram />, label: "Instagram", link: "/" },
-    { icon: <FaFacebook />, label: "Facebook", link: "/" },
-    { icon: <FaYoutube />, label: "YouTube", link: "/" },
+    { 
+        icon: FaTwitter,
+        label: "Twitter", 
+        link: "/" 
+    },
+    { 
+        icon: FaLinkedin, 
+        label: "LinkedIn", 
+        link: "/" 
+    },
+    { 
+        icon: FaInstagram, 
+        label: "Instagram", 
+        link: "/" 
+    },
+    { 
+        icon: FaFacebook, 
+        label: "Facebook", 
+        link: "/" 
+    },
+    { 
+        icon: FaYoutube, 
+        label: "YouTube", 
+        link: "/" 
+    },
 
 ]
 const SocialIcons = () => {
@@ -20,11 +40,11 @@ const SocialIcons = () => {
         <div className="flex gap-2">
             {
 
-                socialData.map( ( { icon, link } ) => (
+                socialData.map( ( { icon: Icon, link } ) => (
 
                     <LinkNewWindow href={ link }>
-                        <div className="flex items-center justify-center p-2 text-lg text-blue-500 bg-white rounded-full hover:text-white hover:bg-orange-500 transition-colors duration-300">
-                            { icon }
+                        <div className="flex items-center justify-center p-2 bg-white rounded-full hover:bg-orange-500 transition-colors duration-300 group">
+                            <Icon className="text-lg text-blue-500 group-hover:text-white duration-300" />
                         </div>
                     </LinkNewWindow> 
 
